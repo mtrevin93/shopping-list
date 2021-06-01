b87dc01630e5c8055fa4e13bf20057b34bd12fa3
mtrevin93 <mtrevin93@gmail.com>
2021-06-01 12:35:37 -0400

    initial commit

---
+++ shopping.js
@@ -0,0 +1,38 @@
+const groceries = [
+    {
+        id: 1,
+        name: "Milk",
+        price: 3.5
+    },
+    {
+        id: 2,
+        name: "Eggs",
+        price: 1.5
+    },
+    {
+        id: 3,
+        name: "Pizza rolls",
+        price: 4
+    },
+    {
+        id: 4,
+        name: "Chicken Breast",
+        price: 6
+    },
+    {
+        id: 5,
+        name: "Pineapple",
+        price: 3.5
+    },
+    {
+        id: 6,
+        name: "Sandwich Buns",
+        price: 3
+    },
+    {
+        id: 7,
+        name: ["Ketchup","Mustard","Mayo"],
+        price: 6
+    }
+]
+console.log(groceries)
